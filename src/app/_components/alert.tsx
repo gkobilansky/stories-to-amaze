f35390b8d58f5a660be1c132636ffcdae8b6102f
Gene <gene@lancekey.com>
2024-11-15 11:13:22 -0500

    fix bug

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -1,5 +1,5 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
+import { REPO_PATH } from "@/lib/constants";
 import cn from "classnames";
 
 type Props = {
@@ -31,7 +31,7 @@ const Alert = ({ preview }: Props) => {
             <>
               The source code for this blog is{" "}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={`https://github.com/gkobilansky/${REPO_PATH}`}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 available on GitHub
